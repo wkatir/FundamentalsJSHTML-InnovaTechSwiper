227fb21af9d11a4ba20356fe2563cef55539e1db
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-11-09 10:47:45 -0600

    feat(about): add fuctionality and style

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,13 +31,14 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-const bgHeader = () =>{
-  const header = document.getElementById('header')
+const bgHeader = () => {
+  const header = document.getElementById("header");
   // Add a class if the bottom offset is greater than 50 of the viewport
-  this.scrollY >= 50 ? header.classList.add('bg-header') 
-                     : header.classList.remove('bg-header')
-}
-window.addEventListener('scroll', bgHeader)
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", bgHeader);
 bgHeader();
 
 /*=============== SWIPER SERVICES ===============*/
