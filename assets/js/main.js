7418506759b24d38a85b025496b7b19fd950c00b
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-11-09 11:33:41 -0600

    feat(services) adding swiper class and styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,20 @@ bgHeader();
 
 /*=============== SWIPER SERVICES ===============*/
 
+const swiperServices = new Swiper('.services__swiper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 24,
+  slidesPerView: "auto",
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
